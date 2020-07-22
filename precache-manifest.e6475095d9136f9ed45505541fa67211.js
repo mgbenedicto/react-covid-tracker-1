d2af3f125d3990bb387191c40acf203b1d8c485d
mgbenedicto <miguelangelo.benedicto@gmail.com>
2020-07-22 20:16:48 +0800

    Updates

---
+++ precache-manifest.e6475095d9136f9ed45505541fa67211.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac7f815a421c75e339ad2bb35558cb72",
+    "url": "/mgbenedicto/react-covid-tracker-1/index.html"
+  },
+  {
+    "revision": "c43a045ff1ef18c6ad3e",
+    "url": "/mgbenedicto/react-covid-tracker-1/static/css/main.980990e4.chunk.css"
+  },
+  {
+    "revision": "e97a5d3777ba74c40953",
+    "url": "/mgbenedicto/react-covid-tracker-1/static/js/2.42e0262a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/mgbenedicto/react-covid-tracker-1/static/js/2.42e0262a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c43a045ff1ef18c6ad3e",
+    "url": "/mgbenedicto/react-covid-tracker-1/static/js/main.20da3a32.chunk.js"
+  },
+  {
+    "revision": "dd11a61d0f498e40dbb6",
+    "url": "/mgbenedicto/react-covid-tracker-1/static/js/runtime-main.19b4052c.js"
+  }
+]);
