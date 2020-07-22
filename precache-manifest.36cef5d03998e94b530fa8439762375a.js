49449f7da88b40394372f1b777ddf5488bb8954f
mgbenedicto <miguelangelo.benedicto@gmail.com>
2020-07-22 21:33:21 +0800

    Updates

---
+++ precache-manifest.36cef5d03998e94b530fa8439762375a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39323eccb66faf92199c2a5a24eab66a",
+    "url": "/react-covid-tracker-1/index.html"
+  },
+  {
+    "revision": "c43a045ff1ef18c6ad3e",
+    "url": "/react-covid-tracker-1/static/css/main.980990e4.chunk.css"
+  },
+  {
+    "revision": "48d5ffcc72e7a4a3b546",
+    "url": "/react-covid-tracker-1/static/js/2.c937a202.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/react-covid-tracker-1/static/js/2.c937a202.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c43a045ff1ef18c6ad3e",
+    "url": "/react-covid-tracker-1/static/js/main.20da3a32.chunk.js"
+  },
+  {
+    "revision": "72885f4687370df9aa3b",
+    "url": "/react-covid-tracker-1/static/js/runtime-main.296e717b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mgbenedicto/react-covid-tracker-1/precache-manifest.e6475095d9136f9ed45505541fa67211.js"
+  "/react-covid-tracker-1/precache-manifest.36cef5d03998e94b530fa8439762375a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mgbenedicto/react-covid-tracker-1/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-covid-tracker-1/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
